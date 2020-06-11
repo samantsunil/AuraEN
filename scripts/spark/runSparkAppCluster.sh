ce893c4c265669150100d9c2ad8980b8e24262ff
SMT\sunil <ssamant@swin.edu.au>
2020-06-12 02:39:55 +1000

    script files added.

--- scripts/spark/runSparkAppCluster.sh
+++ scripts/spark/runSparkAppCluster.sh
@@ -5,6 +5,7 @@ sleep 30
 cd /opt/spark/spark-2.4.5-bin-hadoop2.6/bin
 sudo ./spark-submit \
 --driver-memory 2g \
+--master spark://136.186.108.185:7077
 --total-executor-cores 4 \
 --executor-memory 1g \
 --class "com.iot.app.spark.processor.IoTDataProcessor" \
