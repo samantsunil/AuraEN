775be3ec475fbd40d82c943aed5f8fcc27d699ce
Sunil S Samant <6667076+samantsunil@users.noreply.github.com>
2025-01-30 12:00:41 +1100

    Update createTopic.sh

--- scripts/kafka/createTopic.sh
+++ scripts/kafka/createTopic.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /opt/kafka/bin/
-sudo bash kafka-topics.sh --create --zookeeper 136.186.108.253:2181 --replication-factor 1 --partitions 6 --topic vehicle-data-stream
+sudo bash kafka-topics.sh --create --zookeeper xxx.xxx.xxx.253:2181 --replication-factor 1 --partitions 6 --topic vehicle-data-stream
 cd ~
