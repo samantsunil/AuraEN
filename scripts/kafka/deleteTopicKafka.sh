1375f2cbd402a5051985ca95583804280b28327c
Sunil S Samant <6667076+samantsunil@users.noreply.github.com>
2025-01-30 12:01:02 +1100

    Update deleteTopicKafka.sh

--- scripts/kafka/deleteTopicKafka.sh
+++ scripts/kafka/deleteTopicKafka.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd /opt/kafka/kafka_2.11-2.4.0/bin/
-sudo bash kafka-topics.sh --zookeeper  ec2-3-24-240-124.ap-southeast-2.compute.amazonaws.com:2181 --delete --topic vehicle-data-stream
+sudo bash kafka-topics.sh --zookeeper  ec2-3-xx-xxx-124.ap-southeast-2.compute.amazonaws.com:2181 --delete --topic vehicle-data-stream
 cd ~
