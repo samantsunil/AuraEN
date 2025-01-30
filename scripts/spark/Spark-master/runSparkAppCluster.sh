73b6bf8613824b6f83d88ee3d2aeb6a72450db79
Sunil S Samant <6667076+samantsunil@users.noreply.github.com>
2025-01-30 11:59:31 +1100

    Update runSparkAppCluster.sh

--- scripts/spark/Spark-master/runSparkAppCluster.sh
+++ scripts/spark/Spark-master/runSparkAppCluster.sh
@@ -5,7 +5,7 @@ sleep 30
 cd /opt/spark/spark-2.4.5-bin-hadoop2.6/bin
 sudo ./spark-submit \
 --driver-memory 1024m \
---master spark://136.186.108.185:7077
+--master spark://xxx.xxx.xxx.xxx:7077
 --class "com.iot.app.spark.processor.IoTDataProcessor" \
 /home/ubuntu/iot-spark-processing/target/iot-spark-processor-1.0.0.jar &
 cd ~
