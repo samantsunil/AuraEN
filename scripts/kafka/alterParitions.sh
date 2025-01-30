b45a45d7df96e4168ec0642089179716eaa00c48
Sunil S Samant <6667076+samantsunil@users.noreply.github.com>
2025-01-30 12:00:12 +1100

    Update alterParitions.sh

--- scripts/kafka/alterParitions.sh
+++ scripts/kafka/alterParitions.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 count=$1
 cd /opt/kafka/kafka_2.11-2.4.0/bin/
-sudo bash kafka-topics.sh --alter --zookeeper ec2-3-24-240-124.ap-southeast-2.compute.amazonaws.com:2181 --topic vehicle-data-stream --partitions $count
+sudo bash kafka-topics.sh --alter --zookeeper ec2-3-xx-xxx-124.ap-southeast-2.compute.amazonaws.com:2181 --topic vehicle-data-stream --partitions $count
